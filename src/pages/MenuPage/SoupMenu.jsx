1fccee03e87d827e372b482448244c84b0e34592
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-10-22 02:46:31 +0600

    change button width

--- src/pages/MenuPage/SoupMenu.jsx
+++ src/pages/MenuPage/SoupMenu.jsx
@@ -22,7 +22,7 @@ const SoupMenu = () => {
           <MenuItem key={item._id} item={item} />
         ))}
       </div>
-      <button className="hover:bg-[#1F2937] border-b-4 hover:border-b-4 flex w-36 mx-auto  px-4 py-2 rounded-lg hover:text-white border[#D9D9D9]">
+      <button className="hover:bg-[#1F2937] border-b-4 hover:border-b-4 flex w-60 mx-auto  px-4 py-2 rounded-lg hover:text-white border[#D9D9D9]">
         Ordered Your Favorite food
       </button>
     </section>
